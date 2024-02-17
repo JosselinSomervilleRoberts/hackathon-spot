ef6aaff3dede7a5c109b136f1796ee690f14b024
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-17 12:37:52 -0800

    Hey babe

---
+++ cv_example.py
@@ -0,0 +1,63 @@
+import cv2
+from gtts import gTTS
+import os
+
+
+def detect_faces(frame, face_cascade):
+    # Convert the frame to grayscale for the Haar Cascade detector
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw rectangles around each face
+    for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    return frame
+
+
+def say_something(text: str, file_name: str = "audio.mp3"):
+    myobj = gTTS(text=text, lang="en", slow=False)
+    myobj.save(file_name)
+    os.system(f"ffplay -nodisp -autoexit -loglevel quiet {file_name}")
+
+
+def main():
+    # Load the Haar Cascade for face detection
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
+    # Start the webcam
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Read frame by frame
+        _, frame = cap.read()
+
+        # Convert the frame to grayscale for the Haar Cascade detector
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        if len(faces) > 0:
+            print(f"Found {len(faces)} faces")
+            myobj = gTTS(text="Hey babe", lang="en", slow=False)
+
+            # Saving the converted audio in a mp3 file named
+            # welcome
+            myobj.save("welcome.mp3")
+
+            # Playing the converted file
+            os.system(f"ffplay -nodisp -autoexit -loglevel quiet welcome.mp3")
+            break
+
+        # Break the loop when 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    # Release the VideoCapture object and close windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,50 +1,69 @@
 import os
 import time
 from spot_controller import SpotController
+from gtts import gTTS
+import cv2
 
-ROBOT_IP = "10.0.0.3"#os.environ['ROBOT_IP']
-SPOT_USERNAME = "admin"#os.environ['SPOT_USERNAME']
-SPOT_PASSWORD = "2zqa8dgw7lor"#os.environ['SPOT_PASSWORD']
+ROBOT_IP = "10.0.0.3"  # os.environ['ROBOT_IP']
+SPOT_USERNAME = "admin"  # os.environ['SPOT_USERNAME']
+SPOT_PASSWORD = "2zqa8dgw7lor"  # os.environ['SPOT_PASSWORD']
+
+
+def say_something(text: str, file_name: str = "audio.mp3"):
+    myobj = gTTS(text=text, lang="en", slow=False)
+    myobj.save(file_name)
+    os.system(f"ffplay -nodisp -autoexit -loglevel quiet {file_name}")
 
 
 def main():
-    #example of using micro and speakers
-    print("Start recording audio")
-    sample_name = "aaaa.wav"
-    cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
-    print(cmd)
-    os.system(cmd)
-    print("Playing sound")
-    os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
-    
     # Capture image
-    import cv2
     camera_capture = cv2.VideoCapture(0)
-    rv, image = camera_capture.read()
-    print(f"Image Dimensions: {image.shape}")
-    camera_capture.release()
+
+    # Load the Haar Cascade for face detection
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    say_something("Finished downloading the model")
 
     # Use wrapper in context manager to lease control, turn on E-Stop, power on the robot and stand up at start
     # and to return lease + sit down at the end
-    with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
+    with SpotController(
+        username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP
+    ) as spot:
 
         time.sleep(2)
 
-        # Move head to specified positions with intermediate time.sleep
-        spot.move_head_in_points(yaws=[0.2, 0],
-                                 pitches=[0.3, 0],
-                                 rolls=[0.4, 0],
-                                 sleep_after_point_reached=1)
-        time.sleep(3)
+        say_something("Let me see your face")
+        while True:
+            # Read frame by frame
+            _, frame = camera_capture.read()
 
-        # Make Spot to move by goal_x meters forward and goal_y meters left
-        spot.move_to_goal(goal_x=0.5, goal_y=0)
-        time.sleep(3)
+            # Convert the frame to grayscale for the Haar Cascade detector
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Detect faces
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-        # Control Spot by velocity in m/s (or in rad/s for rotation)
-        spot.move_by_velocity_control(v_x=-0.3, v_y=0, v_rot=0, cmd_duration=2)
-        time.sleep(3)
+            if len(faces) > 0:
+                print(f"Found {len(faces)} faces")
+                say_something("Hey babe")
+
+        # # Move head to specified positions with intermediate time.sleep
+        # spot.move_head_in_points(yaws=[0.2, 0],
+        #                          pitches=[0.3, 0],
+        #                          rolls=[0.4, 0],
+        #                          sleep_after_point_reached=1)
+        # time.sleep(3)
+
+        # # Make Spot to move by goal_x meters forward and goal_y meters left
+        # spot.move_to_goal(goal_x=0.5, goal_y=0)
+        # time.sleep(3)
+
+        # # Control Spot by velocity in m/s (or in rad/s for rotation)
+        # spot.move_by_velocity_control(v_x=-0.3, v_y=0, v_rot=0, cmd_duration=2)
+        # time.sleep(3)
+
+    camera_capture.release()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
